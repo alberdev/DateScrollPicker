65e227ac01e27e8719b95b502ae50ed12669fdb0
Alberto Aznar <albertokr@gmail.com>
2019-11-21 02:23:40 +0100

    First commit

---
+++ Example/DateScrollPicker/Date+Days.swift
@@ -0,0 +1,9 @@
+//
+//  Date+Days.swift
+//  DateScrollPicker_Example
+//
+//  Created by Alberto Aznar de los Ríos on 21/11/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
